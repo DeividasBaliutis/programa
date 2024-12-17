4129c95efbca35f8abe85533d87fd0d852942ba0
DeividasBaliutis <deividasbaliutis@gmail.com>
2024-12-17 16:16:26 +0200

    Add files via upload

--- Projektu_valdymas/programa/views.py
+++ Projektu_valdymas/programa/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Klientas, Darbuotojas, Darbas , Saskaita , Projektas
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    num_klientai = Klientas.objects.all().count()
+    num_darbuotojai = Darbuotojas.objects.all().count()
+    num_darbai = Darbas.objects.count()
+    num_saskaitos = Saskaita.objects.count()
+    num_projektai = Projektas.objects.count()
+
+    # perduodame informaciją į šabloną žodyno pavidale:
+    context = {
+        'num_klientai': num_klientai,
+        'num_darbuotojai': num_darbuotojai,
+        'num_darbai': num_darbai,
+        'num_saskaitos': num_saskaitos,
+        'num_projektai': num_projektai,
+
+    }
+
+    # renderiname index.html, su duomenimis kintamąjame context
+    return render(request, 'index.html', context=context)
